eb20bc5cb24ee6b1c2eb1373c955431d39c3efce
jaeheonYoo325 <yoojh0325@gmail.com>
2022-07-18 00:42:45 +0900

    2022.07.18 : 예제로 구현하는 파일 업로드, 다운로드.

---
+++ src/main/java/com/springmvc2/springmvc2upload/domain/Item.java
@@ -0,0 +1,14 @@
+package com.springmvc2.springmvc2upload.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private UploadFile attachFile;
+    private List<UploadFile> imageFiles;
+}
