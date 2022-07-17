e98e73002b3da1ddcfc5f20dbf1b4883fdab177f
jaeheonYoo325 <yoojh0325@gmail.com>
2022-07-17 22:13:00 +0900

    2022.07.17 : 프로젝트 init.

---
+++ src/main/java/com/springmvc2/springmvc2upload/Springmvc2UploadApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc2.springmvc2upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc2UploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmvc2UploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmvc2/springmvc2upload/Springmvc2UploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc2.springmvc2upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvc2UploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
